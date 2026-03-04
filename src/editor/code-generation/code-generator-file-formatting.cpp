b8db030c1cc43e1edfc348eaff15f3f0fcc6b5de
Arthuryan Lohéac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2026-03-04 11:13:23 +0100

    ✨ Refactor code generation logic to improve newline handling and indentation formatting

--- src/editor/code-generation/code-generator-file-formatting.cpp
+++ src/editor/code-generation/code-generator-file-formatting.cpp
@@ -33,9 +33,35 @@ int countCharOutsideQuotes(const std::string& s, char needle)
         if (!inSingleQuotes && !inDoubleQuotes && c == needle)
             ++count;
     }
-
     return count;
 }
+
+std::vector<std::string> splitByNewlines(const std::string& s)
+{
+    std::vector<std::string> sub_lines = utils::splitByDelims(s, {';'}, true, true);
+    for (int i = 0; i < sub_lines.size(); i++) {
+        if (std::find(sub_lines[i].begin(), sub_lines[i].end(), '\n') == sub_lines[i].end())
+            continue;
+
+        std::vector<std::string> sub_sub_lines = utils::splitByDelims(sub_lines[i], {'\n'}, true, true);
+        sub_lines[i] = "";
+        for (std::string &sub_sub_line : sub_sub_lines) {
+            if (!sub_sub_line.empty())
+                sub_lines[i] += sub_sub_line;
+        }
+    }
+    return sub_lines;
+}
+
+std::string GetNewLIneFormatted(const std::string& line, int indent_level, int current_indent)
+{
+    std::string final_line = "";
+    for (int i = 0; i < current_indent; i++)
+        final_line += std::string(indent_level, ' ');
+    final_line += line + "\n";
+    return final_line;
+}
+
 }  // namespace
 
 std::string CodeGeneratorFile::GetFormatedContent() const
@@ -43,23 +69,17 @@ std::string CodeGeneratorFile::GetFormatedContent() const
     std::string final_code = "";
     // Split the content into code blocks based on curly braces, keeping the delimiters to determine indentation levels.
     std::vector<std::string> code_blocks = utils::splitByDelims(content_, {'{', '}'}, true, true);
-
-    // Track the current indentation level based on the presence of opening and closing braces.
     int current_indent = 0;
 
     for (std::string &line : code_blocks) {
-        std::vector<std::string> sub_lines = utils::splitByDelims(line, {';'}, true, true);
+        std::vector<std::string> sub_lines = splitByNewlines(line);
 
-        // If the line contains a closing brace outside of quotes, decrease the indentation level.
-        current_indent -= countCharOutsideQuotes(line, '}');
         // Parse each line in the code block, applying the current indentation
         for (std::string &sub_line : sub_lines) {
-            for (int i = 0; i < current_indent; i++)
-                final_code += std::string(indent_level_, ' ');
-            final_code += sub_line + "\n";
+            current_indent -= countCharOutsideQuotes(sub_line, '}');
+            final_code += GetNewLIneFormatted(sub_line, indent_level_, current_indent);
+            current_indent += countCharOutsideQuotes(sub_line, '{');
         }
-        // If the line contains an opening brace outside of quotes, increase the indentation.
-        current_indent += countCharOutsideQuotes(line, '{');
     }
     return final_code;
 }
--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -4,8 +4,8 @@
 int main(void) {
   code_generation::CodeGeneratorFile generator;
   generator.AddContent("int main() {");
-  generator.AddContent("    std::cout << \"H;ello, {World}!\" << std::endl;");
-  generator.AddContent("    return 0;");
+  generator.AddContent("std::cout << \"H;ello, {World}!\" << std::endl;");
+  generator.AddContent("return 0;");
   generator.AddContent("}");
   generator.AddContentAt("#include <iostream>\n", 0);
 
