7299b0fb4a25a52eb133af16e62df1b0df5c7320
Arthuryan Lohéac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2026-03-05 11:42:29 +0100

    ✏️Fix norm google

--- src/utils/string-splitter.cpp
+++ src/utils/string-splitter.cpp
@@ -1,26 +1,26 @@
 #include "utils/string-splitter.hpp"
 
 namespace utils {
-std::vector<std::string> splitByDelims(
+std::vector<std::string> SplitByDelims(
     const std::string& input,
     const std::vector<char>& delims,
-    bool keepEmpty,
-    bool keepDelims,
-    bool ignoreDelimsInQuotes
+    bool keep_empty,
+    bool keep_delims,
+    bool ignore_delims_in_quotes
 ) {
     // If no delimiters are provided, return the whole string as a single token.
     if (delims.empty())
         return { input };
 
     // O(1) average lookup to test if a character is a delimiter.
-    std::unordered_set<char> delimSet(delims.begin(), delims.end());
+    std::unordered_set<char> delim_set(delims.begin(), delims.end());
 
     std::vector<std::string> out;
     std::string current;
     current.reserve(input.size());
 
-    bool inSingleQuotes = false;
-    bool inDoubleQuotes = false;
+    bool in_single_quotes = false;
+    bool in_double_quotes = false;
     bool escaping = false;
 
     for (char c : input) {
@@ -30,31 +30,31 @@ std::vector<std::string> splitByDelims(
             continue;
         }
 
-        if ((inSingleQuotes || inDoubleQuotes) && c == '\\') {
+        if ((in_single_quotes || in_double_quotes) && c == '\\') {
             current.push_back(c);
             escaping = true;
             continue;
         }
 
-        if (c == '\'' && !inDoubleQuotes) {
-            inSingleQuotes = !inSingleQuotes;
+        if (c == '\'' && !in_double_quotes) {
+            in_single_quotes = !in_single_quotes;
             current.push_back(c);
             continue;
         }
 
-        if (c == '"' && !inSingleQuotes) {
-            inDoubleQuotes = !inDoubleQuotes;
+        if (c == '"' && !in_single_quotes) {
+            in_double_quotes = !in_double_quotes;
             current.push_back(c);
             continue;
         }
 
-        const bool canSplit = !ignoreDelimsInQuotes || (!inSingleQuotes && !inDoubleQuotes);
+        const bool canSplit = !ignore_delims_in_quotes || (!in_single_quotes && !in_double_quotes);
 
-        if (canSplit && delimSet.find(c) != delimSet.end()) {
+        if (canSplit && delim_set.find(c) != delim_set.end()) {
             // We hit a delimiter: flush the current token.
-            if (keepDelims)
+            if (keep_delims)
                 current.push_back(c);
-            if (keepEmpty || !current.empty()) {
+            if (keep_empty || !current.empty()) {
                 out.push_back(current);
             }
             current.clear();
@@ -65,7 +65,7 @@ std::vector<std::string> splitByDelims(
     }
 
     // Flush the last token.
-    if (keepEmpty || !current.empty())
+    if (keep_empty || !current.empty())
         out.push_back(current);
 
     return out;
--- src/utils/string-splitter.hpp
+++ src/utils/string-splitter.hpp
@@ -4,11 +4,25 @@
 #include <unordered_set>
 
 namespace utils {
-std::vector<std::string> splitByDelims(
+
+/**
+ * @brief Splits the input string into a vector of substrings based on the specified delimiters.
+ * @param input The string to be split.
+ * @param delims A vector of characters to be used as delimiters for splitting the input string.
+ * @param keep_empty If true, empty substrings resulting from consecutive delimiters will be included
+ * in the output vector. If false, they will be excluded.
+ * @param keep_delims If true, the delimiters themselves will be included as separate substrings in the output vector.
+ * If false, the delimiters will not be included in the output vector.
+ * @param ignore_delims_in_quotes If true, delimiters that appear within quoted substrings
+ * will be ignored and not treated as split points. If false, delimiters will be treated as split points regardless of their position in the input string.
+ * @return A vector of substrings resulting from splitting the input string based on the specified delimit
+ * ers and options.
+ */
+std::vector<std::string> SplitByDelims(
     const std::string& input,
     const std::vector<char>& delims,
-    bool keepEmpty = false,
-    bool keepDelims = false,
-    bool ignoreDelimsInQuotes = true
+    bool keep_empty = false,
+    bool keep_delims = false,
+    bool ignore_delims_in_quotes = true
 );
 }  // namespace utils
