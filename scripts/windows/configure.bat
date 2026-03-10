ed13f12526c763527db0a435adf6b02dfc8cd260
mathys-f <mathys.fillion@epitech.eu>
2026-03-10 14:40:11 +0100

    🔨 Update configure.bat to ensure Ninja generator is explicitly specified for CMake

--- scripts/windows/configure.bat
+++ scripts/windows/configure.bat
@@ -11,6 +11,7 @@ echo Configuring Nebula (%BUILD_TYPE%)...
 
 cmake -B %BUILD_DIR% ^
       -S . ^
+      -G Ninja ^
       -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
       -DCPM_SOURCE_CACHE=%CD%\.cache\CPM
 
