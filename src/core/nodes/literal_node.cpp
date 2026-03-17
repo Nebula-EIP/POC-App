4164d79a974bc9c96bb73da3d4a64ce9f5cf8b89
Nolfews <nolan.papa93@gmail.com>
2026-03-17 16:59:27 +0100

    ♻️ Remove InitializeConnections call from LiteralNode constructor for improved initialization control

--- src/core/nodes/literal_node.cpp
+++ src/core/nodes/literal_node.cpp
@@ -4,7 +4,6 @@
 
 core::LiteralNode::LiteralNode(uint32_t id, NodeKind kind) noexcept
     : NodeBase(id, kind) {
-    InitializeConnections();
 }
 
 void core::LiteralNode::InitializeConnections() {
