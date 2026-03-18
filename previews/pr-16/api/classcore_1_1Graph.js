676e1c3034d75d0e11f8426b24506e5eb4beab4d
Nolfews <Nolfews@users.noreply.github.com>
2026-03-18 14:36:26 +0000

    🚀 Deploy preview for pr-16 at 3d4cccfd2929580fa70828240b3b881bb378ac24

--- previews/pr-16/api/classcore_1_1Graph.js
+++ previews/pr-16/api/classcore_1_1Graph.js
@@ -1,8 +1,8 @@
 var classcore_1_1Graph =
 [
+    [ "AddInputPin", "classcore_1_1Graph.html#aaca244a70477893198854e5cb7ed9740", null ],
     [ "AddNode", "classcore_1_1Graph.html#a72cc1dab3878b358dd35630ae1deaa9c", null ],
     [ "AddNode", "classcore_1_1Graph.html#ae3b63f46355c9c6c575f71cba49c7216", null ],
-    [ "AddParameter", "classcore_1_1Graph.html#ac2871d4f1ba15ac5bdf600a0fc5ad411", null ],
     [ "GetAllNodes", "classcore_1_1Graph.html#ad16112a1e0b808902356852caeaa6cf2", null ],
     [ "GetAuthor", "classcore_1_1Graph.html#a0573c1c202b8fce4998285d78904ba1a", null ],
     [ "GetCreatedAt", "classcore_1_1Graph.html#a13bc792dd37fc11853931a2dc7976786", null ],
@@ -12,9 +12,9 @@ var classcore_1_1Graph =
     [ "GetProjectName", "classcore_1_1Graph.html#ad94f2351350b82a034619d0e476eec43", null ],
     [ "GetVersion", "classcore_1_1Graph.html#a8c307eaf4415b9239a6b3d23416ddd89", null ],
     [ "Link", "classcore_1_1Graph.html#a207ca9ea6cd38e2bd6667b62376e765a", null ],
+    [ "RemoveInputPin", "classcore_1_1Graph.html#a7bd2ea23fc0a80ca9b6126d461ad0e1b", null ],
+    [ "RemoveInputPin", "classcore_1_1Graph.html#a7e4d92fa09b408e50d70420e8887b91b", null ],
     [ "RemoveNode", "classcore_1_1Graph.html#aeebc7f038b9f164ad66d34fadac9098a", null ],
-    [ "RemoveParameter", "classcore_1_1Graph.html#a34e08e52f7b7cfc048a87f6e54ebf2d4", null ],
-    [ "RemoveParameter", "classcore_1_1Graph.html#a3b1dc922b197812e72d423f53260a8e2", null ],
     [ "SaveToFile", "classcore_1_1Graph.html#a8934d9981c1b5390a5cfbbb5aff455fe", null ],
     [ "Serialize", "classcore_1_1Graph.html#a61293e2ed581922bc501e5c3dd0409ca", null ],
     [ "SetAuthor", "classcore_1_1Graph.html#a6ba73ea844e5ee5caaed092d5fab16ea", null ],
