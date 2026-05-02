b41931eb6a121ca7275bb6e3a31796b5d7220e6b
mathys-f <mathys-f@users.noreply.github.com>
2026-05-02 15:08:26 +0000

    🚀 Deploy preview for pr-19 at 685cc8685514be65ec54e223f9a1e1160455cdb8

---
+++ previews/pr-19/api/dir_313caf1132e152dd9b58bea13a4052ca.js
@@ -0,0 +1,7 @@
+var dir_313caf1132e152dd9b58bea13a4052ca =
+[
+    [ "exception_base.hpp", "exception__base_8hpp_source.html", null ],
+    [ "id_manager.hpp", "id__manager_8hpp_source.html", null ],
+    [ "logger.hpp", "logger_8hpp_source.html", null ],
+    [ "string-splitter.hpp", "string-splitter_8hpp_source.html", null ]
+];
