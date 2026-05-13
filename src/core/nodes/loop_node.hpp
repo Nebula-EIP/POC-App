afc1108b8ae24976eb98d3c13b3012f93793f946
Nolan Papa <nolan.papa@epitech.eu>
2026-05-13 11:44:48 +0200

    🚨 Refactor LoopNode header for improved formatting and readability

--- src/core/nodes/loop_node.hpp
+++ src/core/nodes/loop_node.hpp
@@ -9,7 +9,7 @@ namespace core {
 class Graph;
 
 class LoopNode : public NodeBase {
-  public:
+   public:
     ~LoopNode() = default;
 
     void SetName(const std::string &name);
@@ -21,23 +21,27 @@ class LoopNode : public NodeBase {
     PinDataType GetInputPinType(uint8_t pin) const;
     PinDataType GetOutputPinType(uint8_t pin) const;
 
-    std::expected<void, std::string> CanConnectTo(uint8_t out_pin, const NodeBase *target, uint8_t in_pin) const noexcept override;
+    std::expected<void, std::string> CanConnectTo(
+        uint8_t out_pin, const NodeBase *target,
+        uint8_t in_pin) const noexcept override;
 
     std::string GetDisplayName() const noexcept override;
     std::string GetCategory() const noexcept override;
 
     nlohmann::json Serialize() const override;
-    std::expected<void, std::string> Deserialize(const nlohmann::json &json) override;
+    std::expected<void, std::string> Deserialize(
+        const nlohmann::json &json) override;
 
-  private:
+   private:
     friend Graph;
     friend NodeBase;
 
-    LoopNode(uint32_t id, NodeKind kind, utils::WrappedVector2 position) noexcept;
+    LoopNode(uint32_t id, NodeKind kind,
+             utils::WrappedVector2 position) noexcept;
     void InitializeConnections() override;
 
-  private:
+   private:
     std::string name_ = "While";
 };
 
-} // namespace core
+}  // namespace core
