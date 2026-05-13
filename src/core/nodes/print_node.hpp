6a41e41e9f4a2189acaff73493f962da76cf17c4
Nolan Papa <nolan.papa@epitech.eu>
2026-05-13 11:44:57 +0200

    🚨 Refactor PrintNode header for improved formatting and readability

--- src/core/nodes/print_node.hpp
+++ src/core/nodes/print_node.hpp
@@ -9,7 +9,7 @@ namespace core {
 class Graph;
 
 class PrintNode : public NodeBase {
-  public:
+   public:
     ~PrintNode() = default;
 
     void SetName(const std::string &name);
@@ -20,24 +20,27 @@ class PrintNode : public NodeBase {
 
     PinDataType GetInputPinType(uint8_t pin) const;
 
-    std::expected<void, std::string> CanConnectTo(uint8_t out_pin, const NodeBase *target,
-                            uint8_t in_pin) const noexcept override;
+    std::expected<void, std::string> CanConnectTo(
+        uint8_t out_pin, const NodeBase *target,
+        uint8_t in_pin) const noexcept override;
 
     std::string GetDisplayName() const noexcept override;
     std::string GetCategory() const noexcept override;
 
     nlohmann::json Serialize() const override;
-    std::expected<void, std::string> Deserialize(const nlohmann::json &json) override;
+    std::expected<void, std::string> Deserialize(
+        const nlohmann::json &json) override;
 
-  private:
+   private:
     friend Graph;
     friend NodeBase;
 
-    PrintNode(uint32_t id, NodeKind kind, utils::WrappedVector2 position) noexcept;
+    PrintNode(uint32_t id, NodeKind kind,
+              utils::WrappedVector2 position) noexcept;
     void InitializeConnections() override;
 
-  private:
+   private:
     std::string name_ = "Print";
 };
 
-} // namespace core
+}  // namespace core
