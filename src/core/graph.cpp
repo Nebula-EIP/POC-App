58e1a02f50d6f0c076e006306e3d533abdfdc08a
Nolan Papa <nolan.papa@epitech.eu>
2026-05-25 14:42:25 +0200

    🚨 Update GetEdges method to specify Edge type in return signature

--- src/core/graph.cpp
+++ src/core/graph.cpp
@@ -1014,7 +1014,7 @@ std::expected<core::Graph, std::string> core::Graph::LoadFromFile(
     }
 }
 
-const std::vector<Edge> &core::Graph::GetEdges() const noexcept {
+const std::vector<core::Graph::Edge> &core::Graph::GetEdges() const noexcept {
     return edges_;
 }
 
