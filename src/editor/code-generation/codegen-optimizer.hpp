159b6707ffbbfd5fa5a4b827700929d3229a0a59
Nolan Papa <nolan.papa@epitech.eu>
2026-05-25 14:32:59 +0200

    🚨 Enhance documentation formatting for DeadCodeAnalysis and CodegenOptimizer methods

--- src/editor/code-generation/codegen-optimizer.hpp
+++ src/editor/code-generation/codegen-optimizer.hpp
@@ -22,8 +22,7 @@ struct TypeInfo {
 
 struct DeadCodeAnalysis {
     std::unordered_set<uint32_t> dead_nodes;
-    std::unordered_map<uint32_t, TypeInfo>
-        type_info;
+    std::unordered_map<uint32_t, TypeInfo> type_info;
 };
 
 class CodegenOptimizer {
@@ -33,14 +32,17 @@ class CodegenOptimizer {
     /**
      * @brief Analyzes the graph for dead code and infers types of node outputs.
      * @param graph The input graph to analyze
-     * @return A DeadCodeAnalysis struct containing sets of dead nodes and type information
+     * @return A DeadCodeAnalysis struct containing sets of dead nodes and type
+     * information
      */
     DeadCodeAnalysis AnalyzeGraph(const core::Graph &graph);
 
     /**
-     * @brief Infers the data types of node outputs based on their connections and usage in the graph.
+     * @brief Infers the data types of node outputs based on their connections
+     * and usage in the graph.
      * @param graph The input graph to analyze
-     * @param type_map An output parameter that will be filled with node ID to PinDataType mappings for each node's output pins
+     * @param type_map An output parameter that will be filled with node ID to
+     * PinDataType mappings for each node's output pins
      */
     void InferTypes(
         const core::Graph &graph,
@@ -54,13 +56,13 @@ class CodegenOptimizer {
     std::unordered_set<uint32_t> FindUsedNodes(const core::Graph &graph);
 
    private:
-
-   /**
-    * @brief Recursively marks nodes as used starting from output nodes and following connections backwards.
-    * @param node_id The ID of the node to mark as used
-    * @param graph The graph context to find connections
-    * @param used_nodes A set that accumulates the IDs of used nodes
-    */
+    /**
+     * @brief Recursively marks nodes as used starting from output nodes and
+     * following connections backwards.
+     * @param node_id The ID of the node to mark as used
+     * @param graph The graph context to find connections
+     * @param used_nodes A set that accumulates the IDs of used nodes
+     */
     void MarkAsUsed(uint32_t node_id, const core::Graph &graph,
                     std::unordered_set<uint32_t> &used_nodes);
 };
