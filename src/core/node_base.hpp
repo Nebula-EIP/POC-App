7c7c7d4a59323987bf0352ea1a4c8da5844ae1f4
Nolan Papa <nolan.papa@epitech.eu>
2026-05-25 11:46:55 +0200

    🚨 Update color attributes in NodeBase to include alpha channel for better rendering

--- src/core/node_base.hpp
+++ src/core/node_base.hpp
@@ -422,8 +422,8 @@ class NodeBase {
     utils::WrappedVector2 initial_position_ = {
         0.0f, 0.0f};  ///< Initial position for resetting
     bool follow_mouse_ = false;
-    utils::WrappedColor color_ = {130, 130, 130};
-    utils::WrappedColor initial_color_ = {130, 130, 130};
+    utils::WrappedColor color_ = {130, 130, 130, 255};
+    utils::WrappedColor initial_color_ = {130, 130, 130, 255};
     bool selected_ = false;
 
     utils::IdManager<uint8_t> in_pin_id_manager_;
