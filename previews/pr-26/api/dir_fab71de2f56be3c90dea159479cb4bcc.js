37e678ec26d7561f06d8cf297c9b4f0ad95c3c49
JeanBizeul <JeanBizeul@users.noreply.github.com>
2026-07-28 07:28:14 +0000

    🚀 Deploy preview for pr-26 at a9588d7c8ee320d7e1e0885b9a36bc2c0686109c

--- previews/pr-26/api/dir_fab71de2f56be3c90dea159479cb4bcc.js
+++ previews/pr-26/api/dir_fab71de2f56be3c90dea159479cb4bcc.js
@@ -1,6 +1,4 @@
 var dir_fab71de2f56be3c90dea159479cb4bcc =
 [
-    [ "datatypes.hpp", "datatypes_8hpp_source.html", null ],
-    [ "graph.hpp", "new__core_2graph_8hpp_source.html", null ],
-    [ "node.hpp", "node_8hpp_source.html", null ]
+    [ "graph", "dir_13ff9625ce1189f15286c6e7b7b5f90b.html", "dir_13ff9625ce1189f15286c6e7b7b5f90b" ]
 ];
