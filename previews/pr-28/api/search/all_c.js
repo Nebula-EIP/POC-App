4809f8f0d2e365484b49c6cf22a6b394708db1e1
ArthuryanLoheac <ArthuryanLoheac@users.noreply.github.com>
2026-08-04 16:50:11 +0000

    🚀 Deploy preview for pr-28 at 8cbc45d53828ce0fa097817e959184b2c0f4df0c

---
+++ previews/pr-28/api/search/all_c.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['name_0',['name',['../structcore_1_1NodeBase_1_1OutputPin.html#a899c3b71b139940672d7b18e35db8714',1,'core::NodeBase::OutputPin::name'],['../classcore_1_1ConditionNode.html#a1226b126cf94dc4e0e32ecf321a6ca45',1,'core::ConditionNode::Name()'],['../classcore_1_1ForNode.html#a8df67162172c3ebcf0868bc15a22a3c6',1,'core::ForNode::Name()'],['../classcore_1_1LoopNode.html#afb972e432a224f60f935bd18c04683f3',1,'core::LoopNode::Name()'],['../classcore_1_1PrintNode.html#ad53f236ce9dd7609cdebf849ca06fcb3',1,'core::PrintNode::Name()']]],
+  ['newid_1',['NewId',['../classutils_1_1IdManager.html#a082864f4bb110efa1c2fe259e5819626',1,'utils::IdManager']]],
+  ['node_2',['node',['../structcore_1_1NodeBase_1_1Connection.html#ad90a8713cd8cc49cf749d4b0135a548b',1,'core::NodeBase::Connection']]],
+  ['nodebase_3',['nodebase',['../classcore_1_1NodeBase.html',1,'core::NodeBase'],['../classcore_1_1NodeBase.html#a92ac57f0faa7668730a9557249829691',1,'core::NodeBase::NodeBase(uint32_t id, NodeKind kind, utils::WrappedVector2 position={0.0f, 0.0f}) noexcept']]],
+  ['nodekind_4',['NodeKind',['../classcore_1_1NodeBase.html#a7a37bc942dfb1b307307d7bc7a5b934e',1,'core::NodeBase']]],
+  ['nodenotfoundexception_5',['NodeNotFoundException',['../classcore_1_1NodeNotFoundException.html',1,'core']]]
+];
