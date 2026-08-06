e98b3482568d392f0c6ba28f1715f2e55cfa62fe
mathys-f <mathys.fillion@epitech.eu>
2026-08-06 18:34:53 +0200

    ✨ Implement INodeListCapability API header and base ICapability

--- src/new_core/modules/capabilities/node_list_capability.hpp
+++ src/new_core/modules/capabilities/node_list_capability.hpp
@@ -1,9 +1,74 @@
-namespace core {
+/**
+ * @file node_list_capability.hpp
+ * @brief Defines the capability for a module to provide custom nodes.
+ *
+ * @author Created by Antigravity
+ * @date Created on 06-08-2026
+ *
+ * @author Last modified by Antigravity
+ * @date Last modified on 06-08-2026
+ */
 
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "../../graph/datatypes.hpp"
+#include "../icapability.hpp"
+
+namespace core {
 namespace capa {
 
-class INodeListCapability;
+/**
+ * @brief Metadata describing a node provided by a module.
+ */
+struct NodeMetadata {
+    NodeType type;            ///< Unique identifier for the node type within the module.
+    std::string name;         ///< Human-readable name of the node.
+    std::string description;  ///< Short description of the node's behavior.
+};
+
+/**
+ * @brief Configuration data required to build a Node.
+ */
+struct NodeConfiguration {
+    std::vector<Pin> input_pins;    ///< The input pins for the node.
+    std::vector<Pin> output_pins;   ///< The output pins for the node.
+    PropertyMap default_properties; ///< The default properties the node should be initialized with.
+};
+
+/**
+ * @brief Interface for the capability providing custom nodes.
+ */
+class INodeListCapability : public ICapability {
+   public:
+    virtual ~INodeListCapability() = default;
+
+    /**
+     * @brief Queries the list of all nodes provided by this module.
+     *
+     * @return A list of metadata for all available nodes.
+     */
+    virtual std::vector<NodeMetadata> getAvailableNodes() const noexcept = 0;
+
+    /**
+     * @brief Initializes the node list capability with the resolved property types.
+     *
+     * @param property_types A map associating property names to their corresponding PropertyTypeId. Required.
+     */
+    virtual void initializePropertyTypes(const std::unordered_map<std::string, PropertyTypeId>& property_types) = 0;
+
+    /**
+     * @brief Retrieves the layout and default configuration for a specific node type.
+     *
+     * @param type The NodeType identifier to get the configuration for. Required.
+     *
+     * @return The configuration needed to build the specified node.
+     */
+    virtual NodeConfiguration getNodeConfiguration(NodeType type) const = 0;
+};
 
 }  // namespace capa
-
 }  // namespace core
---
+++ src/new_core/modules/icapability.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file icapability.hpp
+ * @brief Base interface for all module capabilities.
+ *
+ * @author Created by Antigravity
+ * @date Created on 06-08-2026
+ *
+ * @author Last modified by Antigravity
+ * @date Last modified on 06-08-2026
+ */
+
+#pragma once
+
+namespace core {
+
+/**
+ * @brief Base interface for all module capabilities.
+ */
+class ICapability {
+   public:
+    virtual ~ICapability() = default;
+};
+
+}  // namespace core
