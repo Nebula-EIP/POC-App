5f57f863321ea5b5636853153e779c62da73a57e
JeanBizeul <jean.bizeul@epitech.eu>
2026-08-11 17:06:35 +0200

    ✨ Add uncommented type list capability file

--- src/new_core/modules/capabilities/type_list_capability.hpp
+++ src/new_core/modules/capabilities/type_list_capability.hpp
@@ -1,8 +1,33 @@
+#pragma once
+
+#include <string>
+#include <span>
+#include <unordered_map>
+
+#include "datatypes.hpp"
+
 namespace core {
 
 namespace capa {
 
-class ITypeListCapability;
+class ITypeListCapability {
+    struct TypeDefinition {
+        DataType id;
+        std::string_view name;
+    };
+   public:
+    virtual ~ITypeListCapability() = 0;
+
+    virtual bool registerType(DataType type_id) const noexcept = 0;
+
+    virtual DataType typeId(std::string_view type_name) const noexcept = 0;
+    virtual std::string_view typeName(DataType type_id) const noexcept = 0;
+
+    virtual std::span<const TypeDefinition> types() const noexcept = 0;
+
+   private:
+    std::unordered_map<DataType, std::string_view> types;
+};
 
 }  // namespace capa
 
