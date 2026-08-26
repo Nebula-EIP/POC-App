f1a453eb8f47e8ffaec6a97a02dda1f12979a6d8
JeanBizeul <jean.bizeul@epitech.eu>
2026-08-26 23:29:16 +0200

    📝 Add comments in datatypes.hpp

--- src/new_core/graph/datatypes.hpp
+++ src/new_core/graph/datatypes.hpp
@@ -1,5 +1,12 @@
 /**
- * This files contains all datatypes widely used in the core lib
+ * @file datatypes.cpp
+ * @brief Declares all types commonly used in the core library
+ *
+ * @author Created by JeanBizeul
+ * @date Created on 01-08-2026
+ *
+ * @author Last modified by JeanBizeul
+ * @date Last modified on 26-08-2026
  */
 
 #pragma once
@@ -13,14 +20,17 @@
 #include <vector>
 
 namespace core {
-using NodeId = uint32_t;          // Unique id for each Node
-using PinId = uint16_t;           // Unique id for each Node's Pin
-using DataType = uint16_t;        // Unique id for each type declared by modules
-using NodeType = uint16_t;        // Unique id for each node declared by modules
-using ModuleId = uint8_t;         // Unique id for each loaded module
-using PropertyId = uint16_t;      // Unique id for each Node's properties
-using PropertyTypeId = uint32_t;  // Unique id for each property type
+using NodeId = uint32_t;          ///< Unique id for each Node
+using PinId = uint16_t;           ///< Unique id for each Node's Pin
+using DataType = uint16_t;        ///< Unique id for each type declared by modules
+using NodeType = uint16_t;        ///< Unique id for each node declared by modules
+using ModuleId = uint8_t;         ///< Unique id for each loaded module
+using PropertyId = uint16_t;      ///< Unique id for each Node's properties
+using PropertyTypeId = uint32_t;  ///< Unique id for each property type
 
+/**
+ * @brief Represents a connection between two nodes
+ */
 struct Connection {
     NodeId from_node;
     PinId from_pin;  // aka output pin
@@ -31,15 +41,21 @@ struct Connection {
     DataType data_type;
 };
 
+/**
+ * @brief Represents a connection point of a node
+ */
 struct Pin {
     PinId id;
     std::string name;
     DataType type;
 };
 
+/**
+ * @brief Represents a type
+ */
 struct Property {
     PropertyTypeId type_id;
-    std::any value;
+    std::any value;     ///< Free storage space for the property's values
 };
 
 using PropertyMap = std::unordered_map<PropertyId, Property>;
