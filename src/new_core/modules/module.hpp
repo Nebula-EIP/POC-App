01865475d62559f53a2e3481d3b37a2c5e5597a6
JeanBizeul <jean.bizeul@epitech.eu>
2026-09-02 17:11:07 +0200

    💡 Add comments to the module.hpp file

--- src/new_core/modules/module.hpp
+++ src/new_core/modules/module.hpp
@@ -1,3 +1,15 @@
+/**
+ * @file module.hpp
+ * @brief Module interface. Modules are loaded by the module loader
+ * and provide capabilities to the core.
+ *
+ * @author Created by JeanBizeul
+ * @date Created on 02-09-2026
+ *
+ * @author Last modified by JeanBizeul
+ * @date Last modified on 02-09f-2026
+ */
+
 #pragma once
 
 #include <cstdint>
@@ -22,48 +34,124 @@ class IModule {
    public:
     virtual ~IModule() = default;
 
+    /**
+     * @brief Get the name of the module.
+     *
+     * @return The name of the module.
+     */
     virtual std::string_view name() const noexcept = 0;
-    virtual std::string_view author() const noexcept;
-    virtual std::string_view description() const noexcept;
 
+    /**
+     * @brief Get the author of the module.
+     *
+     * @return The author of the module.
+     */
+    virtual std::string_view author() const noexcept = 0;
+
+    /**
+     * @brief Get the description of the module.
+     *
+     * @return The description of the module.
+     */
+    virtual std::string_view description() const noexcept = 0;
+
+    /**
+     * @brief Version wrapper
+     */
     struct Version {
         uint16_t major;
         uint16_t minor;
         uint16_t patch;
     };
+
+    /**
+     * @brief Get the version of the module.
+     *
+     * @return The version of the module.
+     */
     virtual Version version() const noexcept = 0;
 
     /**
-     * @brief The id in the param here is just to inform the module of the id
-     * the loader has given him.
+     * @brief Initialize the module
+     *
+     * @param id The id of the module, assigned by the core. Required.
+     *
+     * @return true if the module has been initialized, false if not
      */
     virtual bool initialize(ModuleId id) = 0;
+
+    /**
+     * @brief Ask the module to release all resources and prepare for shutdown.
+     */
     virtual void shutdown() noexcept = 0;
 
+    /**
+     * @brief Get the id of the module.
+     *
+     * @return The id of the module.
+     */
     virtual ModuleId id() const noexcept = 0;
 
     /// Mendatory capabilities
 
+    /**
+     * @brief Get the type list capability of the module.
+     *
+     * @return The type list capability of the module..
+     */
     virtual capa::ITypeListCapability *types() = 0;
+
+    /**
+     * @brief Get the node list capability of the module.
+     *
+     * @return The node list capability of the module.
+     */
     virtual capa::INodeListCapability *nodes() = 0;
 
-    virtual ICapability *capability(std::type_index type) noexcept = 0;
-    virtual const ICapability *capability(
-        std::type_index type) const noexcept = 0;
-
+    /**
+     * @brief Get a capability by type.
+     *
+     * @param T The type of the capability to get.
+     *
+     * @return A pointer to the capability, or nullptr if not found.
+     */
     template <typename T>
-    T *capability() noexcept {
-        static_assert(std::is_base_of_v<ICapability, T>);
-        return static_cast<T *>(capability(typeid(T)));
-    }
+    T *capability() noexcept;
 
+    /**
+     * @brief Get a const capability by type.
+     *
+     * @param T The type of the capability to get.
+     *
+     * @return A const pointer to the capability, or nullptr if not found.
+     */
     template <typename T>
-    const T *capability() const noexcept {
-        static_assert(std::is_base_of_v<ICapability, T>);
-        return static_cast<const T *>(capability(typeid(T)));
-    }
+    const T *capability() const noexcept;
 
    private:
+    /**
+     * @brief Get a capability by type.
+     * Internal method, use the template version instead.
+     *
+     * @param type The type of the capability to get.
+     *
+     * @return A pointer to the capability, or nullptr if not found.
+     */
+    virtual ICapability *capability(std::type_index type) noexcept = 0;
+
+    /**
+     * @brief Get a const capability by type.
+     * Internal method, use the template version instead.
+     *
+     * @param type The type of the capability to get.
+     *
+     * @return A const pointer to the capability, or nullptr if not found.
+     */
+    virtual const ICapability *capability(
+        std::type_index type) const noexcept = 0;
 };
 
 }  // namespace core
+
+// Retreive the implementation of the module template functions
+#include "module.tcc"
---
+++ src/new_core/modules/module.tcc
@@ -0,0 +1,24 @@
+#pragma once
+
+// For IDE  only
+#include "module.hpp"
+
+namespace core {
+
+// These functions allow safer use of the internal capability() methods,
+// by using the typeid of the template parameter instead of a std::type_index
+
+
+template <typename T>
+T *IModule::capability() noexcept {
+    static_assert(std::is_base_of_v<ICapability, T>);
+    return static_cast<T *>(capability(typeid(T)));
+}
+
+template <typename T>
+const T *IModule::capability() const noexcept {
+    static_assert(std::is_base_of_v<ICapability, T>);
+    return static_cast<const T *>(capability(typeid(T)));
+}
+
+}  // namespace core
