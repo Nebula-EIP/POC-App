435932a6f24e031f3b31bcaf05e379ceb7ae3123
JeanBizeul <jean.bizeul@epitech.eu>
2026-09-02 17:13:01 +0200

    💡 Add comments to create_module.hpp

--- src/new_core/modules/create_module.hpp
+++ src/new_core/modules/create_module.hpp
@@ -1,5 +1,11 @@
 #include "module.hpp"
 
 extern "C" {
+
+/**
+ * @brief Create a new module instance.
+ *
+ * @return A pointer to the new module, or nullptr if creation failed.
+ */
 core::IModule *CreateModule();
 }
