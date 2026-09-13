3dbe6899604e553b7d9bcd6bdb9c4ca60449abc3
nathan bezard <nathan.bezard@epitech.eu>
2026-09-13 15:22:12 +0200

    ✨ Add exporter_capability.cpp file

---
+++ src/core/modules/capabilities/exporter_capability.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file exporter_capability.cpp
+ * @brief Implementation of the exporter capability
+ *
+ * @author Created by Nathan Bezard
+ * @date Created on 13-09-2026
+ *
+ * @author Last modified by Nathan Bezard
+ * @date Last modified on 13-09-2026
+ */
+
+#include "exporter_capability.hpp"
+
+namespace core::capa {
+    
+}
