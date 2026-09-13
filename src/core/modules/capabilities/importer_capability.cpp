b2169ef0ffcb73faee2f416ba25f76f4c9c578f7
Nolfews <nolan.papa93@gmail.com>
2026-09-13 18:55:25 +0200

    🚨 Fix ImportCode method to use correct request variable

--- src/core/modules/capabilities/importer_capability.cpp
+++ src/core/modules/capabilities/importer_capability.cpp
@@ -34,7 +34,7 @@ std::expected<void, ImportError> ImporterCapability::ImportCode(
                         "No importer handler is configured"});
     }
 
-    return handler_(request);
+    return handler_(request_);
 }
 
 }  // namespace core::capa
