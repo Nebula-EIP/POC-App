e288c502f044801673b21e5e9d93ea3a1082919f
JeanBizeul <jean.bizeul@epitech.eu>
2026-09-17 11:21:20 +0200

    ✨ Add exception for the type list capability

---
+++ src/core/exception/capabilities_exception/capabilities_exceptions.md
@@ -0,0 +1,9 @@
+# Capabilities Exceptions
+
+These exceptions are destined to the capabilities defined in the core lib.
+
+# Type List Capability Exceptions
+
+## DuplicateTypeNameException
+
+Thrown when trying to create a type with an already attributed name in the constructor of the TypeListCapability implementation of the core lib.
---
+++ src/core/exception/capabilities_exception/type_list_capability_exception.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file type_list_capability_exception.hpp
+ * @brief Exceptions thrown by the TypeListCapability class
+ *
+ * @author Created by JeanBizeul
+ * @date Created on 17-09-2026
+ *
+ * @author Last modified by JeanBizeul
+ * @date Last modified on 17-09-2026
+ */
+
+#include "exception/exception.hpp"
+
+namespace core {
+
+/**
+ * @brief Thrown when trying to create a type with an already assigned name
+ */
+class DuplicateTypeNameException : public Exception {
+   public:
+    using Exception::Exception;
+};
+
+} // namespace core
