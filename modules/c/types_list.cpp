aa75c0f8da7705c32809fac9c2ff4828800f99fc
Nolan PAPA <146716503+Nolfews@users.noreply.github.com>
2026-09-20 20:06:29 +0200

    ✨ Update type list capability constructor to register types and improve documentation

--- modules/c/types_list.cpp
+++ modules/c/types_list.cpp
@@ -1,17 +1,22 @@
 /**
  * @file init_types.cpp
- * @brief Contains the constructor which builds the capa containing all types from C module
+ * @brief Contains the constructor which builds the capa containing all types
+ * from C module
  *
  * @author Created by JeanBizeul
  * @date Created on 14-09-2026
  *
- * @author Last modified by JeanBizeul
- * @date Last modified on 14-09-2026
+ * @author Last modified by Nolan Papa
+ * @date Last modified on 20-09-2026
  */
 
 #include "modules_inits.hpp"
 
-core::capa::TypeListCapability *CreateTypeListCapa()
-{
-    return new core::capa::TypeListCapability();
+core::capa::TypeListCapability *CreateTypeListCapa() {
+    auto *types = new core::capa::TypeListCapability();
+    types->RegisterType("int");
+    types->RegisterType("double");
+    types->RegisterType("bool");
+    types->RegisterType("char");
+    return types;
 }
