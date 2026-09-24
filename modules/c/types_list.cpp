f82cc47750cb42ca5f8dd198f150dca69814231f
Arthuryan Lohéac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2026-09-24 15:40:56 +0900

    ✨ Refactor TypeListCapability initialization and update test setup for C types

--- modules/c/types_list.cpp
+++ modules/c/types_list.cpp
@@ -6,17 +6,26 @@
  * @author Created by JeanBizeul
  * @date Created on 14-09-2026
  *
- * @author Last modified by Nolan Papa
- * @date Last modified on 20-09-2026
+ * @author Last modified by ArthuryanLoheac
+ * @date Last modified on 24-09-2026
  */
 
 #include "modules_inits.hpp"
 
+namespace {
+
+/// Names of the C types, in the order the core registers them.
+const std::vector<std::string_view> kTypesNames = {
+    "void",    ///> Void/Null
+    "int",     ///> Integer
+    "float",   ///> Floating point number
+    "bool",    ///> Boolean
+    "char",    ///> Character - uint8_t
+    "string",  ///> string pointer
+};
+
+}  // namespace
+
 core::capa::TypeListCapability *CreateTypeListCapa() {
-    auto *types = new core::capa::TypeListCapability();
-    types->RegisterType("int");
-    types->RegisterType("double");
-    types->RegisterType("bool");
-    types->RegisterType("char");
-    return types;
+    return new core::capa::TypeListCapability(kTypesNames);
 }
